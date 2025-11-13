102b2958f225c997d87ee045767188e02762da2d
Anya Solace Ebubechi <jesusislord640@gmail.com>
2025-11-13 13:12:42 +0100

    Update and rename PROJECT.sql to Menu_items.sql

--- Menu_items.sql
+++ Menu_items.sql
@@ -1,9 +1,12 @@
+-- View the menu_items table
 SELECT * FROM [PROJECT].[dbo].[menu_items];
 
+-- Find the number of items on the menu
 SELECT COUNT(*)
 FROM 
   [PROJECT].[dbo].[menu_items];
 
+-- What are the least and most expensive items on the menu
 SELECT * FROM [PROJECT].[dbo].[menu_items]
 ORDER BY 
    CAST(price AS FLOAT) DESC;
@@ -12,11 +15,12 @@ SELECT * FROM [PROJECT].[dbo].[menu_items]
 ORDER BY 
    CAST(price AS FLOAT) ASC;
 
+-- How many Italian dishes are on the menu?
 SELECT COUNT(category)
 FROM [PROJECT].[dbo].[menu_items]
 WHERE category = 'Italian'
 
-
+-- What are the least and most expensive Italian dishes on the menu
 SELECT *
 FROM [PROJECT].[dbo].[menu_items]
 WHERE category = 'Italian'
@@ -28,5 +32,16 @@ FROM [PROJECT].[dbo].[menu_items]
 WHERE category = 'Italian'
 ORDER BY 
    CAST(price AS FLOAT) DESC
+
+-- How many dishes are in each category
+SELECT category, COUNT(menu_item_id) AS num_dishes
+FROM [PROJECT].[dbo].[menu_items]
+GROUP BY category;
+
+-- What's the average dish price within each category?
+SELECT category, AVG(CAST(price AS FLOAT)) AS average_dish_price 
+FROM [PROJECT].[dbo].[menu_items]
+GROUP BY category;
+
 
 
